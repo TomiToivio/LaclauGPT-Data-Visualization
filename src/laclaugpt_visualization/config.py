9fcafab560cae6be4b2592d95791702e65ac6374
Tomi Toivio (ψ/acc) <tomi.toivio@helsinki.fi>
2026-09-17 03:18:14 +0300

    Preserve direct redis_url settings compatibility

--- src/laclaugpt_visualization/config.py
+++ src/laclaugpt_visualization/config.py
@@ -41,6 +41,7 @@ class Settings(BaseSettings):
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
+        populate_by_name=True,
     )
 
     project_id: str = "default"
